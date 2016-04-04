f939452f53c57ab5fcde392e1a9689f0271ed88b
debodirno <debodirnochandra@gmail.com>
2016-04-05 00:10:13 +0530

    "Dialer setup"

--- app/src/main/java/xenophobe/applications/com/dialerplus/MainActivity.java
+++ app/src/main/java/xenophobe/applications/com/dialerplus/MainActivity.java
@@ -8,30 +8,19 @@ import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
-
 public class MainActivity extends Activity implements View.OnClickListener {
     private EditText screen;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initializeView();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     private void initializeView() {
@@ -89,8 +78,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 display("#");
                 break;
             case R.id.dial:
-                if (screen.getText().length() > 0 && checkCallPermission()) {
-                    startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + screen.getText().toString())));
+                if (screen.getText().toString().isEmpty())
+                    ;
+                else if (checkCallPermission()) {
+                    startActivity(new Intent(Intent.ACTION_CALL,Uri.parse("tel:" + screen.getText())));
+                    Log.d("CALLS", screen.getText().toString());
                 }
                 break;
             case R.id.imageButton:
@@ -103,49 +95,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
         }
     }
 
-    public boolean checkCallPermission() {
+    public boolean checkCallPermission(){
         String permission = "android.permission.CALL_PHONE";
         int res = getApplicationContext().checkCallingOrSelfPermission(permission);
         return (res == PackageManager.PERMISSION_GRANTED);
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://xenophobe.applications.com.dialerplus/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://xenophobe.applications.com.dialerplus/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }
 }
